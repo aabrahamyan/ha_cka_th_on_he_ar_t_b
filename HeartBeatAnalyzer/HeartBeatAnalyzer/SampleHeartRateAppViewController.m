f97a3c22c0010ab12cdb56c7d5083644472ec321
Armen Abrahamyan <armen.abrahamyan@sourcio2.com>
2013-09-14 01:42:25 +0400

    Project File Merge

--- HeartBeatAnalyzer/HeartBeatAnalyzer/SampleHeartRateAppViewController.m
+++ HeartBeatAnalyzer/HeartBeatAnalyzer/SampleHeartRateAppViewController.m
@@ -307,7 +307,6 @@
     });
 }
 
-
 // r,g,b values are from 0 to 1 // h = [0,360], s = [0,1], v = [0,1] 
 //	if s == 0, then h = -1 (undefined) 
 void RGBtoHSV( float r, float g, float b, float *h, float *s, float *v ) {
